07b2ed37f9c298e7f29b8a74f15df64ae8ee73d2
AyoubNL <gmilio@live.nl>
2024-04-16 03:59:20 +0200

    bij opdracht 2b

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -1,11 +1,8 @@
 // Je gaat functies schrijven die we kunnen hergebruiken om een lijst met eindcijfers van studenten te checken. Je zult over de cijfers heen moeten itereren (hoe pak je dat aan?),
 // maar ook een manier moeten vinden om hetgeen dat je verzamelt ergens te bundelen. Op deze manier zul je ontdekken hoe je omgaat met scope. Pak vooral het hoofdstuk op EdHub over for-loops er nog eens bij!
 // Tip: je mag hier geen ingebouwde object methoden gebruiken, dus daar hoef je niet naar te kijken.
-
 const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
-
 /* Opdracht  1: Cum Laude */
-
 /* 1a: Script schrijven  */
 // De administratie moet weten hoeveel studenten er dit blok cum laude zijn afgestudeerd (8 of hoger). Daar moeten namelijk speciale diploma's voor besteld worden.
 // Schrijf de stapjes om dit te kunnen checken eerst uit en vraag jezelf de volgende dingen af:
@@ -14,29 +11,35 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // * Hoe zorgt ik ervoor dat wanneer ik een cijfer tegenkom die aan de conditie voldoet, ik dit ergens kan bijhouden?
 // Log het antwoord in de terminal.
 // ---- Verwachte uitkomst: 6
-
-function cumLaude(students) {
-    for (let i = 0; i < students.length; i++) {
-        if (students[i] >= 8) {
-            console.log(students[i])
-        }
+let cumLaudeCounter = 0
+for (let i = 0; i < grades.length; i++) {
+    if (grades[i] >= 8) {
+        cumLaudeCounter = cumLaudeCounter + 1
     }
 }
-cumLaude(grades)
-
+// console.log(cumLaudeCounter)
 
 /*  1b: Omschrijven tot een herbruikbare functie   */
 // Schrijf een functie genaamd cumLaude, die een array van cijfers verwacht (zoals grades) en het aantal Cum laude studenten teruggeeft. Gebruik hiervoor jouw antwoord van 1a.
 // Zorg ervoor dat jouw functie ook werkt als we een andere array met eindcijfers willen checken, zoals bijvoorbeeld: [6, 4, 5] of [8, 9, 4, 6, 10].
 // Log het antwoord in de terminal.
+const novi = [6, 4, 5];
+const hva = [8, 9, 4, 6, 10];
 
-// ---- Verwachte uitkomsten:
-// cumLaude(grades) geeft 6
-// cumLaude([6, 4, 5]) geeft 0
-// cumLaude([8, 9, 4, 6, 10]) geeft 3
+let totalCum = 0
 
-// function cumLaude(){}
+function cumLaude(students) {
+    for (let i = 0; i < students.length; i++) {
+        if (students[i] >= 8) {
+            totalCum = totalCum + 1
+        }
+    }
+    console.log(totalCum)
+}
 
+// cumLaude(grades)
+// cumLaude(novi)
+// cumLaude(hva)
 
 /* Opdracht  2: Gemiddeld cijfer */
 
@@ -49,6 +52,13 @@ cumLaude(grades)
 // Log het antwoord in de terminal.
 
 // ---- Verwachte uitkomst: 6.642857142857143
+let amount = 0
+let total = 0
+for (let i = 0; i < grades.length; i++) {
+    amount++
+    total += grades[i]
+}
+console.log(total / amount)
 
 
 /* 2b: Omschrijven tot een herbruikbare functie */
--- functions-methods.js
+++ functions-methods.js
@@ -8,15 +8,15 @@
 // getEmailDomain("n.eeken@novi-education.nl") geeft novi-education.nl
 // getEmailDomain("t.mellink@novi.nl") geeft novi.nl
 // getEmailDomain("a.wiersma@outlook.com") geeft outlook.com
-//
-// let email = []
-//
-// function getEmailDomain(email){
-//     console.log(email.slice(email.indexOf('@') +1));
-// }
-// getEmailDomain("n.eeken@novi-education.nl")
-// getEmailDomain("t.mellink@novi.nl")
-// getEmailDomain("a.wiersma@outlook.com")
+
+let email = []
+
+function getEmailDomain(email){
+    console.log(email.slice(email.indexOf('@') +1));
+}
+getEmailDomain("n.eeken@novi-education.nl")
+getEmailDomain("t.mellink@novi.nl")
+getEmailDomain("a.wiersma@outlook.com")
 
 /* Opdracht  2 */
 // Schrijf een functie genaamd typeOfEmail, die een emailadres verwacht. De functie checkt of het emailadres een novi domein heeft (medewerker), een novi-education domein (student), of extern domein (zoals gmail of outlook)
@@ -25,23 +25,23 @@
 // typeOfEmail("t.mellink@novi.nl") geeft geeft "Medewerker"
 // typeOfEmail("novi.nlaapjesk@outlook.com") geeft geeft "Extern" <-- deze moet het ook doen!
 // typeOfEmail("a.wiersma@outlook.com") geeft "Extern"
-// let emailadres = []
-// function typeOfEmail(emailadres) {
-//     if (emailadres.includes('@novi-education.nl')) {
-//         console.log('Student')
-//     }
-//     else if (emailadres.includes('@novi.nl')) {
-//         console.log('Medewerker')
-//     }
-//     else {
-//         console.log('Extern')}
-//
-// }
-//
-// typeOfEmail("n.eeken@novi-education.nl")
-// typeOfEmail("t.mellink@novi.nl")
-// typeOfEmail("novi.nlaapjesk@outlook.com")
-// typeOfEmail("a.wiersma@outlook.com")
+let emailadres = []
+function typeOfEmail(emailadres) {
+    if (emailadres.includes('@novi-education.nl')) {
+        console.log('Student')
+    }
+    else if (emailadres.includes('@novi.nl')) {
+        console.log('Medewerker')
+    }
+    else {
+        console.log('Extern')}
+
+}
+
+typeOfEmail("n.eeken@novi-education.nl")
+typeOfEmail("t.mellink@novi.nl")
+typeOfEmail("novi.nlaapjesk@outlook.com")
+typeOfEmail("a.wiersma@outlook.com")
 
 
 /* Opdracht  3 */
@@ -60,6 +60,7 @@
 let mail = ''
 function checkEmailValidity(mail) {
     console.log((mail.includes('@') && !(mail.includes(',') || (mail.endsWith('.')))))}
+
 checkEmailValidity("n.eeken@novi.nl")
 checkEmailValidity("tessmellink@novi.nl")
 checkEmailValidity("n.eekenanovi.nl")
